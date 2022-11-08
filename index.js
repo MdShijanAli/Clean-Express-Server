5c33c481cb6e6d25145ca046807373446676e77c
FreelancerShijan <freelancershijan@gmail.com>
2022-11-08 06:01:07 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Slean Service Api Running')
+})
+
+// username: cleanDBUser
+// pass: xnsP3pTJl3Qjz6lT
+
+
+
+const uri = "mongodb+srv://cleanDBUser:xnsP3pTJl3Qjz6lT@cluster0.jqheb6c.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on: ${port}`)
+})
